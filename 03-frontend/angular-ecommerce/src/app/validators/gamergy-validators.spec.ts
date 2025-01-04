8544e2242a084f273b74b5246a7f278d96968ea6
MishoEEP <145448876+MishoEEP@users.noreply.github.com>
2025-01-04 22:40:38 +0100

    [EcommerceApp] Added Validation for all form fields, including credit card digits and security code

---
+++ 03-frontend/angular-ecommerce/src/app/validators/gamergy-validators.spec.ts
@@ -0,0 +1,7 @@
+import { GamergyValidators } from './gamergy-validators';
+
+describe('GamergyValidators', () => {
+  it('should create an instance', () => {
+    expect(new GamergyValidators()).toBeTruthy();
+  });
+});
