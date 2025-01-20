c4385cb6af986aec01d998503ca19fca34949538
MishoEEP <145448876+MishoEEP@users.noreply.github.com>
2025-01-20 23:28:48 +0100

    [EcommerceApp] refactored the backend - handle customers by email (only 1 identical email), added configuration - CORS  Mapping, protected the customer data

--- 02-backend/spring-boot-ecommerce/src/main/java/com/misho/ecommerce/controller/CheckoutController.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/misho/ecommerce/controller/CheckoutController.java
@@ -6,7 +6,6 @@ import com.misho.ecommerce.dto.PurchaseResponse;
 import com.misho.ecommerce.service.CheckoutService;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
