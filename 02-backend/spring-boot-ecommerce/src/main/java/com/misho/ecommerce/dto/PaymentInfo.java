4a8d214ff90df4f949bf8889d7bbff15ceb07e3a
MishoEEP <145448876+MishoEEP@users.noreply.github.com>
2025-01-27 20:20:12 +0100

    [EcommerceApp] implemented backend part of payment processing with Stripe

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/misho/ecommerce/dto/PaymentInfo.java
@@ -0,0 +1,11 @@
+package com.misho.ecommerce.dto;
+
+import lombok.Data;
+
+@Data
+public class PaymentInfo {
+
+    private int amount;
+    private String currency;
+
+}
--- 02-backend/spring-boot-ecommerce/src/main/java/com/misho/ecommerce/service/CheckoutService.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/misho/ecommerce/service/CheckoutService.java
@@ -1,10 +1,14 @@
 package com.misho.ecommerce.service;
 
+import com.misho.ecommerce.dto.PaymentInfo;
 import com.misho.ecommerce.dto.Purchase;
 import com.misho.ecommerce.dto.PurchaseResponse;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 public interface CheckoutService {
 
     PurchaseResponse placeOrder(Purchase purchase);
 
+    PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException;
 }
